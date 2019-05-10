e75ab038ea625b0605724169f06880e48642a93d
Yu Wu <u6250866@anu.edu.au>
2019-05-10 21:51:29 +1000

    add Board, Gameinit, Point! (u6250866)

---
+++ app/src/main/java/com/example/u6250082/myapplication/Point.java
@@ -0,0 +1,17 @@
+package com.example.u6250082.myapplication;
+//u6250866
+public class Point {
+    int i;
+    int j;
+
+    Point(int i, int j){
+        this.i=i;
+        this.j=j;
+    }
+
+    int geti(){ return i; }
+    int getj(){ return j; }
+    void seti() {this.i=i;}
+    void setj() {this.j=j;}
+
+}
