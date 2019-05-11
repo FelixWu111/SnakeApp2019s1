98297e9dd62db26a43ba680ba29a6111f53a7388
Xuguang Song <u6250082@anu.edu.au>
2019-05-11 15:48:12 +1000

    change position logic

--- app/src/main/java/com/example/u6250082/myapplication/Gameinit.java
+++ app/src/main/java/com/example/u6250082/myapplication/Gameinit.java
@@ -6,30 +6,35 @@ public class Gameinit {
     public static final int xboard =18;
     public static final int yboard =27;
 
-    ArrayList<Point> outside = new ArrayList<>();
+    ArrayList<Position> outside = new ArrayList<>();
 
     public Gameinit(){ }
 
     public void firstStep(){
         buildw();
     }
-    //u6250866
+    //u6250866 and u6250082
+    /* 0, 1, 2,.. 17
+    *  18, 19,.. 35
+    *  36, 37,......
+    *  468........ 485*/
     void buildw(){
         for (int i =0;i<xboard;i++){
             if (i==0){
                 for(int j=0; j<yboard;j++){ //for left wall
-                    outside.add(new Point(0,j));
+                    outside.add(new Position(j*18));
                 }
             }else if (i == xboard-1){
                 for(int l=0; l<yboard;l++){//for right wall
-                    outside.add(new Point(xboard-1,l));
+                    outside.add(new Position(l*18+17));
                 }
             }else {
-                outside.add(new Point(i,1)); //THE TOP WALL STARTS FROM INDEX 1!
-                outside.add(new Point(i,yboard-1));
+                outside.add(new Position(i+18)); //THE TOP WALL STARTS FROM INDEX 1!
+                outside.add(new Position(468+i));
             }
         }
     }
+
     //u6250866
     public Board[] getboardState(){
         int index = xboard*yboard;
@@ -39,8 +44,8 @@ public class Gameinit {
             boardState[z]= Board.Path;
         }
 
-        for (Point p: outside ){
-            boardState[p.getj()*18+p.geti()]= Board.Edge;//we list all points in the points to edge.
+        for (Position p: outside ){
+            boardState[p.geti()]= Board.Edge;//we list all points in the points to edge.
         }
         return boardState;
     }
--- app/src/main/java/com/example/u6250082/myapplication/Position.java
+++ app/src/main/java/com/example/u6250082/myapplication/Position.java
@@ -1,17 +1,14 @@
 package com.example.u6250082.myapplication;
 //u6250866
-public class Point {
-    int i;
-    int j;
+public class Position {
 
-    Point(int i, int j){
+    int i;
+
+    Position(int i){
         this.i=i;
-        this.j=j;
     }
 
     int geti(){ return i; }
-    int getj(){ return j; }
     void seti() {this.i=i;}
-    void setj() {this.j=j;}
 
 }
