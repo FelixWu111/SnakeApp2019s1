2d92dcde19a454160ede7729dff871c9b202b64a
Qingzheng Xu <u6174243@anu.edu.au>
2019-05-20 16:06:21 +1000

    Help

--- app/src/main/java/com/example/u6250082/myapplication/LoginActivity.java
+++ app/src/main/java/com/example/u6250082/myapplication/LoginActivity.java
@@ -4,6 +4,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+
 //Qingzheng XU
 //u6174243
 public class LoginActivity extends AppCompatActivity {
@@ -17,5 +20,19 @@ public class LoginActivity extends AppCompatActivity {
     public void start(View v) {//Start tourist mode
         Intent i = new Intent(LoginActivity.this, MainActivity.class);
         startActivity(i);
+    }
+
+    public void help(View v){//Open the instruction
+        findViewById(R.id.instruction).setVisibility(View.VISIBLE);
+        findViewById(R.id.back).setVisibility(View.VISIBLE);
+    }
+
+    public void back(View v){//Close the instruction
+        final Animation out = new AlphaAnimation(1.0f, 0.0f);
+        out.setDuration(3000); // fade animation
+        findViewById(R.id.instruction).startAnimation(out);
+        findViewById(R.id.back).startAnimation(out);
+        findViewById(R.id.instruction).setVisibility(View.INVISIBLE);
+        findViewById(R.id.back).setVisibility(View.INVISIBLE);
     }
 }
